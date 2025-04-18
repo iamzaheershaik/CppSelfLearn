1fdadadbb524bfbcc2a900bf919c75ae18c97c23
zaheershaik <zaheershaik0323@gmail.com>
2025-04-18 21:32:27 +0530

    CompoundAssignAdded

---
+++ Programs/CompoundAssign.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int sum = 10, sub = 15, div = 20, mult = 25, mod = 30; 
+	int x = 5;
+	 
+	sum+=x;
+	cout<<sum<<endl;
+
+	sub-=x;
+	cout<<sub<<endl;
+
+	div/=x;
+	cout<<div<<endl;
+
+	mult*=x;
+	cout<<mult<<endl;
+
+	mod%=x;
+	cout<<mod<<endl;
+
+	return 0;
+
+}
