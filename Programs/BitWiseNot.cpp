a11c123bf210c192b4f9d766149bd6c0be2ee094
zaheershaik <zaheershaik0323@gmail.com>
2025-04-19 00:06:27 +0530

    BitWiseAdded

---
+++ Programs/BitWiseNot.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	char x =5, y;
+	y=~x;
+	cout<<(int)y<<endl;
+	//in Left Value Divided by 2 
+}
---
+++ Programs/BitWiseXor.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	int x = 11, y = 7, z;
+
+	z=x^y;
+	cout<<z<<endl;
+}
