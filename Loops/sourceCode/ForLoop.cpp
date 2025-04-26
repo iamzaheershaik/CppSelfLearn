1343eac88f624e741c6dbca969037a7babe3b796
zaheershaik <zaheershaik0323@gmail.com>
2025-04-26 21:18:53 +0530

    Firstcommit

---
+++ Loops/sourceCode/ForLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ int main (){
+ 	int n,i;
+ 	cout<<"Enter the Value of n";
+ 	cin>>n;
+ 	for(i=0;i<n;i++){
+ 		cout<<i<<" 俺の名前はモンキー・D・ルフィ 海賊王になるんだ "<<endl;
+ 		//My Name is Monkey D Luffy I am gonna be king of pirates
+ 	}
+ }
