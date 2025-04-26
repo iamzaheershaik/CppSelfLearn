814b2325043ffc26d2fe291802874105068f6f9e
zaheershaik <zaheershaik0323@gmail.com>
2025-04-26 21:43:41 +0530

    infiniteloop

---
+++ Loops/sourceCode/Loopbreak.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n , i=0;
+	cout<<"Enter the Value of N :";
+	cin>>n;
+	for(;;){
+		cout<<i<<"Roronoa Zoro"<<endl;
+		i++;
+		if(i>n){
+			break;
+		}
+	}
+}
---
+++ Loops/sourceCode/infiniteLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, i;
+	cout<<"Enter the Value of N"<<endl;
+	cin>>n;
+	for(;;){
+		cout<<"Monkey D Luffy"<<endl;
+	}
+}
