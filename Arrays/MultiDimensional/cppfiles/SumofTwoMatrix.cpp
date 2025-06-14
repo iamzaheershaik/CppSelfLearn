a8851f30a9183a0d7fccdbe7adbbeee7573f8b58
zaheershaik <zaheershaik0323@gmail.com>
2025-06-14 21:39:41 +0530

    MultiDArrayAdded

--- Arrays/MultiDimensional/cppfiles/SumofTwoMatrix.cpp
+++ Arrays/MultiDimensional/cppfiles/SumofTwoMatrix.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
 using namespace std;
-
-int main() {
+int main(){
     int A[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     int B[3][3]={{9,8,7},{6,5,4},{3,2,1}};
     int C[3][3];
-    for (auto &x:C) {
-        for (auto &y:x) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<3; j++) {
             C[i][j]=A[i][j]*B[i][j];
         }
         cout<<endl;
     }
-    for (auto &x:C) {
-        for (auto &y:x) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<3; j++) {
             cout<<C[i][j]<<" ";
         }
         cout<<endl;
     }
-
 }
